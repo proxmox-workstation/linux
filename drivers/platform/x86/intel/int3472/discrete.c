d5496cf4a72d5b9e70d036eca823e199fa2d90b1
Denis Benato <benato.denis96@gmail.com>
2025-03-25 12:29:22 +0000

    Revert "UBUNTU: SAUCE: platform/x86: int3472: Add handshake GPIO function"
    This reverts commit 4a88cd9601c7dbfb5636aef0ba78c677e40137a2.

--- drivers/platform/x86/intel/int3472/discrete.c
+++ drivers/platform/x86/intel/int3472/discrete.c
@@ -190,10 +190,6 @@ static void int3472_get_func_and_polarity(struct acpi_device *adev, u8 *type,
 		*func = "power-enable";
 		*gpio_flags = GPIO_ACTIVE_HIGH;
 		break;
-	case INT3472_GPIO_TYPE_HANDSHAKE:
-		*func = "handshake";
-		*polarity = GPIO_ACTIVE_HIGH;
-		break;
 	default:
 		*func = "unknown";
 		*gpio_flags = GPIO_ACTIVE_HIGH;
@@ -286,12 +282,6 @@ static int skl_int3472_handle_gpio_resources(struct acpi_resource *ares,
 	case INT3472_GPIO_TYPE_RESET:
 	case INT3472_GPIO_TYPE_POWERDOWN:
 		ret = skl_int3472_map_gpio_to_sensor(int3472, agpio, func, gpio_flags);
-		if (ret)
-			err_msg = "Failed to map GPIO pin to sensor\n";
-
-		break;
-	case INT3472_GPIO_TYPE_HANDSHAKE:
-		ret = skl_int3472_map_gpio_to_sensor(int3472, agpio, func, polarity);
 		if (ret)
 			err_msg = "Failed to map GPIO pin to sensor\n";
 
